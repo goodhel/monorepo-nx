2b3453239c7b2b600c6fef335b0e8d8dc6507a8a
Yudhi Kusuma <yudhie123@gmail.com>
2023-03-02 08:31:33 +0700

    refactor: change approach for backend server

--- packages/server/src/controllers/auth.ts
+++ packages/server/src/controllers/auth.ts
@@ -1,103 +1,15 @@
-import prisma from '../helpers/db'
-import { publicProcedure, router } from '../trpc'
-import { z } from 'zod'
+import { router } from '../trpc'
 import m$auth from '../modules/auth.module'
-import response from '../helpers/response'
-import { TRPCError } from '@trpc/server'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import { debug } from '../config/app.config.json'
 
 const authRouter = router({
   /**
    * Register User
    */
-  register: publicProcedure
-    .input(z.object({
-      email: z.string(),
-      password: z.string(),
-      name: z.string()
-    }))
-    .mutation(async ({ input }) => {
-      const register = await m$auth.register(input)
-
-      return response.sendResponse(register)
-    }),
+  register: m$auth.register,
   /**
    * Login User
    */
-  login: publicProcedure
-    .input(z.object({
-      email: z.string(),
-      password: z.string()
-    }))
-    .mutation(async ({ input }) => {
-      try {
-        const schema = z.object({
-          email: z.string().email().transform((val) => val.toLowerCase()),
-          password: z.string().min(6)
-        }).safeParse(input)
-
-        if (!schema.success) {
-          const formatted = schema.error.issues.map((issue) => issue.message).join(', ')
-
-          throw new TRPCError({
-            code: 'BAD_REQUEST',
-            message: formatted
-          })
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email: input.email
-          },
-          select: {
-            id: true,
-            email: true,
-            name: true,
-            password: true
-          }
-        })
-
-        if (!user) {
-          throw new TRPCError({
-            code: 'NOT_FOUND',
-            message: 'User not found'
-          })
-        }
-
-        if (!bcrypt.compareSync(input.password, user.password)) {
-          throw new TRPCError({
-            code: 'UNAUTHORIZED',
-            message: 'Wrong password'
-          })
-        }
-
-        const payload = {
-          id: user.id,
-          email: user.email
-        }
-
-        const token = jwt.sign(payload, 'secret-key-123', { expiresIn: '8h' })
-
-        return {
-          status: true,
-          data: {
-            test: 'test',
-            token
-          }
-        }
-      } catch (error: any) {
-        if (debug) {
-          console.error('login auth module Error: ', error)
-        }
-
-        throw new TRPCError({
-          code: error.code ? error.code : 'BAD_REQUEST',
-          message: error.message
-        })
-      }
-    })
+  login: m$auth.login
 })
 
 export default authRouter
--- packages/server/src/modules/auth.module.ts
+++ packages/server/src/modules/auth.module.ts
@@ -3,158 +3,142 @@ import { debug } from '../config/app.config.json'
 import { z } from 'zod'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import { User } from '@prisma/client'
-
-interface LoginType {
-    email: string
-    password: string
-}
-
-interface RegisterType {
-    email: string
-    password: string
-    name: string
-}
-
-interface LoginOutput {
-  status: boolean
-  code?: string
-  data?: { token: string }
-  error?: string
-}
-
-interface RegisterOutput {
-  status: boolean
-  code?: string
-  data?: User
-  error?: string
-}
+import { publicProcedure } from '../trpc'
+import { TRPCError } from '@trpc/server'
 
 class _auth {
-  login = async (body: LoginType): Promise<LoginOutput> => {
-    try {
-      const schema = z.object({
-        email: z.string().email().transform((val) => val.toLowerCase()),
-        password: z.string().min(6)
-      }).safeParse(body)
+  register = publicProcedure
+    .input(z.object({
+      email: z.string(),
+      password: z.string(),
+      name: z.string()
+    }))
+    .mutation(async ({ input }) => {
+      try {
+        const schema = z.object({
+          email: z.string().email().transform((val) => val.toLowerCase()),
+          password: z.string().min(6),
+          name: z.string().min(2)
+        }).safeParse(input)
 
-      if (!schema.success) {
-        const formatted = schema.error.issues.map((issue) => issue.message).join(', ')
+        if (!schema.success) {
+          const formatted = schema.error.issues.map((issue) => issue.message).join(', ')
 
-        return {
-          status: false,
-          code: 'BAD_REQUEST',
-          error: formatted
-        }
-      }
-
-      const user = await prisma.user.findUnique({
-        where: {
-          email: body.email
-        },
-        select: {
-          id: true,
-          email: true,
-          name: true,
-          password: true
-        }
-      })
-
-      if (!user) {
-        return {
-          status: false,
-          code: 'NOT_FOUND',
-          error: 'User not found'
-        }
-      }
-
-      if (!bcrypt.compareSync(body.password, user.password)) {
-        return {
-          status: false,
-          code: 'UNAUTHORIZED',
-          error: 'Wrong password'
-        }
-      }
-
-      const payload = {
-        id: user.id,
-        email: user.email
-      }
-
-      const token = jwt.sign(payload, 'secret-key-123', { expiresIn: '8h' })
-
-      return {
-        status: true,
-        data: {
-          token
-        }
-      }
-    } catch (error: any) {
-      if (debug) {
-        console.error('login auth module Error: ', error)
-      }
-
-      return {
-        status: false,
-        error: error.message
-      }
-    }
-  }
-
-  register = async (body: RegisterType): Promise<RegisterOutput> => {
-    try {
-      const schema = z.object({
-        email: z.string().email().transform((val) => val.toLowerCase()),
-        password: z.string().min(6),
-        name: z.string().min(2)
-      }).safeParse(body)
-
-      if (!schema.success) {
-        const formatted = schema.error.issues.map((issue) => issue.message).join(', ')
-
-        return {
-          status: false,
-          code: 'BAD_REQUEST',
-          error: formatted
-        }
-      }
-
-      const register = await prisma.user.create({
-        data: {
-          email: body.email,
-          password: bcrypt.hashSync(body.password, 10),
-          name: body.name,
-          user_role: {
-            create: {
-              role_id: 2
-            }
+          return {
+            status: false,
+            code: 'BAD_REQUEST',
+            error: formatted
           }
         }
-      })
 
-      return {
-        status: true,
-        data: register
-      }
-    } catch (error: any) {
-      if (error.code === 'P2002') {
+        const register = await prisma.user.create({
+          data: {
+            email: input.email,
+            password: bcrypt.hashSync(input.password, 10),
+            name: input.name,
+            user_role: {
+              create: {
+                role_id: 2
+              }
+            }
+          }
+        })
+
+        return {
+          status: true,
+          data: register
+        }
+      } catch (error: any) {
+        if (error.code === 'P2002') {
+          return {
+            status: false,
+            code: 'CONFLICT',
+            error: 'Email already exists'
+          }
+        }
+
+        if (debug) {
+          console.error('register auth module Error: ', error)
+        }
+
         return {
           status: false,
-          code: 'CONFLICT',
-          error: 'Email already exists'
+          error: error.message
         }
       }
+    })
 
-      if (debug) {
-        console.error('register auth module Error: ', error)
-      }
+  login = publicProcedure
+    .input(z.object({
+      email: z.string(),
+      password: z.string()
+    }))
+    .mutation(async ({ input }) => {
+      try {
+        const schema = z.object({
+          email: z.string().email().transform((val) => val.toLowerCase()),
+          password: z.string().min(6)
+        }).safeParse(input)
 
-      return {
-        status: false,
-        error: error.message
+        if (!schema.success) {
+          const formatted = schema.error.issues.map((issue) => issue.message).join(', ')
+
+          throw new TRPCError({
+            code: 'BAD_REQUEST',
+            message: formatted
+          })
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: input.email
+          },
+          select: {
+            id: true,
+            email: true,
+            name: true,
+            password: true
+          }
+        })
+
+        if (!user) {
+          throw new TRPCError({
+            code: 'NOT_FOUND',
+            message: 'User not found'
+          })
+        }
+
+        if (!bcrypt.compareSync(input.password, user.password)) {
+          throw new TRPCError({
+            code: 'UNAUTHORIZED',
+            message: 'Wrong password'
+          })
+        }
+
+        const payload = {
+          id: user.id,
+          email: user.email
+        }
+
+        const token = jwt.sign(payload, 'secret-key-123', { expiresIn: '8h' })
+
+        return {
+          status: true,
+          data: {
+            token
+          }
+        }
+      } catch (error: any) {
+        if (debug) {
+          console.error('login auth module Error: ', error)
+        }
+
+        throw new TRPCError({
+          code: error.code ? error.code : 'BAD_REQUEST',
+          message: error.message
+        })
       }
-    }
-  }
+    })
 }
 
 export default new _auth()
